dd0b1a0f04233367bcd3b19b5675d641e48f8c7d
Nayla <nayla.putri53@ui.ac.id>
2026-09-09 22:03:39 +0700

    Implement Tutorial 02: Model-View-Template (MVT) on Django

--- main/views.py
+++ main/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from main.models import Experience
 
-# Create your views here.
+def show_main(request):
+    context = {
+        "name": "Nayla",
+        "npm": "2506657182",
+        "study_program": "S1 Sistem Informasi",
+        "bio": (
+            "Information Systems student at Universitas Indonesia passionate "
+            "about web development, technology solutions, and building impactful digital products."
+        ),
+    }
+    return render(request, "index.html", context)
+
+def show_experience(request):
+    context = {
+        "name": "Nayla",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
